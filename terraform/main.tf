66f612300f801e1713f1fed15660c1e259dc61ef
Tal <tfarin1000@gmail.com>
2025-07-24 20:09:30 +0300

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,13 @@ resource "aws_instance" "builder" {
     subnet_id              = data.aws_subnets.target_vpc_subnets.ids[0]
     key_name               = aws_key_pair.builder_key.key_name
     vpc_security_group_ids = [aws_security_group.builder_sg.id]
+    associate_public_ip_address = true
+
     tags = {
+        Name = "builder"
         Owner = "tal"
     }
+
     provisioner "remote-exec" {
       inline = [
         "sudo apt update -y",
@@ -15,13 +19,11 @@ resource "aws_instance" "builder" {
         "sudo systemctl start docker",
         "sudo usermod -aG docker ubuntu" #maybe need to log out and in of the user
         ]
-
-        
-        connection {
-            type        = "ssh"
-            user        = "ubuntu"
-            private_key = file("${path.module}/builder_key.pem")
-            host        = self.public_ip
-        }
+    }
+    connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/builder_key.pem")
+    host        = self.public_ip
     }
 }
